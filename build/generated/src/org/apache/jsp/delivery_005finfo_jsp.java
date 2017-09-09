142cfa9098f3e4f31f7ca5fcf2a2356290e5c37d
Amal <wickramarathna.amal@gmail.com>
2017-09-09 07:53:59 +0530

    bugs fix

---
+++ build/generated/src/org/apache/jsp/delivery_005finfo_jsp.java
@@ -0,0 +1,130 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+
+public final class delivery_005finfo_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>Delivery Info</title>\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("        <script type=\"text/javascript\">\n");
+      out.write("            window.location.hash = \"no-back-button\";\n");
+      out.write("            window.location.hash = \"Again-No-back-button\";//again because google chrome don't insert first hash into history\n");
+      out.write("            window.onhashchange = function() {\n");
+      out.write("                window.location.hash = \"no-back-button\";\n");
+      out.write("            }\n");
+      out.write("        </script>\n");
+      out.write("        <link rel=\"stylesheet\" href=\"com.official.cazzendra.css.common/bootstrap.min.css\">\n");
+      out.write("        \n");
+      out.write("\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <div style=\"position: absolute; left: 0px; top: 0px; width: 100%; height: 100%;\">\n");
+      out.write("            <div style=\"position: absolute; left: 10%; top: 5px; width: 200px; height: 50px; \">\n");
+      out.write("                <a href=\"admin_panel.jsp\" style=\"font-family: serif; font-size: 24px;\"><<  Control Panel</a> \n");
+      out.write("            </div>\n");
+      out.write("            \n");
+      out.write("            <div  class=\"table container\" style=\"position: absolute; left: 10%; top: 10%; width: 80%; height: 100%; \">\n");
+      out.write("                <table class=\"table table-responsive\" >\n");
+      out.write("                    <tr>\n");
+      out.write("\n");
+      out.write("                        \n");
+      out.write("                        <th style=\"background-color: #99ccff;  width: 200px; height: 30px; color: #000000; font-size: 18px; font-weight: 300; \" >Name</th>\n");
+      out.write("                        <th style=\"background-color: #99ccff;  width: 200px; height: 30px; color: #000000; font-size: 18px; font-weight: 300; \" >Address L1</th>\n");
+      out.write("                        <th style=\"background-color: #99ccff;  width: 200px; height: 30px; color: #000000; font-size: 18px; font-weight: 300; \" >Address L2</th>\n");
+      out.write("                        <th style=\"background-color: #99ccff;  width: 200px; height: 30px; color: #000000; font-size: 18px; font-weight: 300; \" >City</th>\n");
+      out.write("              \n");
+      out.write("                        <th style=\"background-color: #99ccff;  width: 200px; height: 30px; color: #000000; font-size: 18px; font-weight: 300; \" >Amount</th>\n");
+      out.write("                        <th style=\"background-color: #99ccff;  width: 200px; height: 30px; color: #000000; font-size: 18px; font-weight: 300; \" >Status</th>\n");
+      out.write("                        <th style=\"background-color: #99ccff;  width: 200px; height: 30px; color: #000000; font-size: 18px; font-weight: 300; \" >---</th>\n");
+      out.write("\n");
+      out.write("                    </tr>\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("                    <form method=\"POST\" action=\"update_dlvr\" >\n");
+      out.write("                        <tr class=\"table-condensed\">\n");
+      out.write("                            \n");
+      out.write("                          \n");
+      out.write("                            <td>\n");
+      out.write("                                <input class=\"btn btn-danger\"  type=\"submit\" name=\"btn-33\" value=\"Deliver\"/>\n");
+      out.write("                                <input type=\"hidden\" name=\"btn3\" value=\"\"/>\n");
+      out.write("                           </td>\n");
+      out.write("                           \n");
+      out.write("                           \n");
+      out.write("                           \n");
+      out.write("                            \n");
+      out.write("                        </tr>\n");
+      out.write("                    </form>\n");
+      out.write("                  \n");
+      out.write("\n");
+      out.write("                </table>\n");
+      out.write("            </div>\n");
+      out.write("         \n");
+      out.write("        </div>\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ web/quickMenu.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : quickMenu
+    Created on : Sep 8, 2017, 11:18:09 PM
+    Author     : 4m4l
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+    </head>
+    <body>
+       
+    </body>
+</html>
